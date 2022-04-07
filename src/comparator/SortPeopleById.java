835468786fc07fe6662319491aac731f2829500f
Edu <edumartsil95@tutanota.com>
2022-04-07 10:25:14 +0200

    Comparator finished

---
+++ src/comparator/SortPeopleById.java
@@ -0,0 +1,10 @@
+package comparator;
+
+import java.util.Comparator;
+
+public class SortPeopleById implements Comparator<Person> {
+    @Override
+    public int compare(Person o1, Person o2) {
+        return o1.getId()- o2.getId();
+    }
+}
